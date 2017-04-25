1688bd3e462b2dad9fa19ae1e8686b41ee7b4ffe
Maël Guiraud <maelguiraud@hotmail.fr>
2017-04-25 18:35:08 +0200

    results

--- code/simons.h
+++ code/simons.h
@@ -3,3 +3,5 @@ int simons(Graphe g, int taille_paquet, int TMAX, int mode_test, int mode);
 int is_ok(Graphe g, int taille_paquet, int * mi, int * wi);
 void fisher_yates(int * tab, int taille);
 void tri_bulles_inverse(int* tab,int* ordre,int taille);
+int greater(int * tab,int taille);
+int lower(int * tab,int taille);
