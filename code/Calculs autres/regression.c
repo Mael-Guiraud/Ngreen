7486fb532fdfe6053b2578f9ace912605bff654a
Maël Guiraud <maelguiraud@hotmail.fr>
2017-07-24 15:30:56 +0200

    regression

--- code/Calculs autres/regression.c
+++ code/Calculs autres/regression.c
@@ -1,16 +1,16 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <math.h>
-#define nb_points 12
+#define nb_points 13
 
 
-int x[nb_points];
-int y[nb_points];
+float x[nb_points];
+float y[nb_points];
 
 
 void lire_fichier(int algo_choisi)
 {
-	FILE * f = fopen("results_periodea.data","r");
+	FILE * f = fopen("../datas/results_periode.data","r");
 
 	int nombre_lu;
 	int colonne=0;
@@ -18,13 +18,16 @@ void lire_fichier(int algo_choisi)
 	while(fscanf(f,"%d",&nombre_lu))
 	{
 		if(colonne == 0)
-			x[ligne] = nombre_lu;
+			x[ligne] = (float)nombre_lu;
 
 		if(colonne == algo_choisi)
-			y[ligne] = nombre_lu;
+		{
+			y[ligne] = (x[ligne]*2500)/((float)nombre_lu);
+			//printf("%f %f \n",x[ligne], y[ligne]);
+		}
 		colonne++;
 
-		if(colonne == 6)
+		if(colonne == 7)
 		{
 			colonne = 0;
 			ligne ++;
@@ -37,20 +40,20 @@ void lire_fichier(int algo_choisi)
 }
 
 
-void affiche_tab(int *tab)
+void affiche_tab(float *tab)
 {
 	printf("[");
 	for(int i=0;i<nb_points;i++)
 	{
 		if(i < nb_points-1)
-			printf("%d;",tab[i]);
+			printf("%f;",tab[i]);
 		else
-			printf("%d]",tab[i]);
+			printf("%f]",tab[i]);
 	}
 	printf("\n");
 }
 
-float moyenne(int * tab)
+float moyenne(float * tab)
 {
 	float moy=0;
 	for(int i =0;i<nb_points;i++)
@@ -59,7 +62,7 @@ float moyenne(int * tab)
 	}
 	return moy/nb_points;
 }
-float variance(int * tab)
+float variance(float * tab)
 {
 	float var = 0;
 	for(int i=0;i<nb_points;i++)
@@ -97,10 +100,10 @@ float coefficient_correlation()
 }
 int main()
 {
-	for(int i=1;i<4;i++)
+	for(int i=1;i<5;i++)
 	{
 		lire_fichier(i);
-		printf("L'équation de la droite est : y = %f x + %f.\n Le coefficient de corélation est : %f.\n\n",coef_dir(),ordonee_origine(),coefficient_correlation());
+		printf("L'équation de la droite est : y = %f x + %f.\n Le coefficient de corélation est : %f.\n Moyenne = %f.\n\n",coef_dir(),ordonee_origine(),coefficient_correlation(),moyenne(y));
 
 	}
 }
